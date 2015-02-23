3518571cad289da656e5dd71a5806e45934b4822
Nicholas Tountasakis <ntountas@umich.edu>
2015-02-22 20:37:56 -0500

    Added level where can draw platforms
    Draw platforms using the same idea as the triangles. Except clicking
    spacebar, click the mouse to set the bottom left then top right points
    of the cube. Colors alternate until you’re out of platforms.

--- Assets/drawPoint.cs
+++ Assets/drawPoint.cs
@@ -1,15 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class drawPoint : MonoBehaviour {
 	public Transform mesh;
 	public Transform vertex;
 	public Transform[] vertices;
+	public int numPlatformsAllowed;
+	public Material cyan;
+	public Material red;
+	
+	public Text platformInfo;
+	public Text scoreKeeper;
+	
+	private List<Vector3> platformLoc;
+	private List<GameObject> platforms;
 
 	// Use this for initialization
 	void Start () {
 		vertices = new Transform[2];
+		platformLoc = new List<Vector3>();
+		platforms = new List<GameObject>();
+		platformInfo.text = (numPlatformsAllowed-platforms.Count)+" pfrms left, drawing cyan";
 	}
 	
 	// Update is called once per frame
@@ -33,6 +46,60 @@ public class drawPoint : MonoBehaviour {
 
 			// Add new vertex to mesh
 			mesh.GetComponent<mesh>().meshMaker(previewPos);
+		}
+		else if(Input.GetMouseButtonDown(0)){
+			recordPositions();
+		}
+	}
+	void drawPlatform()
+	{
+		if(platformLoc.Count != 0 && (platformLoc.Count % 2 == 0))
+		{
+			int _index = platformLoc.Count - 2;
+			Vector3 pos0 = platformLoc[_index++];
+			Vector3 pos1 = platformLoc[_index];
+			float absX0 = Mathf.Abs(pos0.x);
+			float absX1 = Mathf.Abs(pos1.x);
+			float xSize = Mathf.Max (absX0,absX1) - Mathf.Min (absX0,absX1);
+			
+			float absY0 = Mathf.Abs(pos0.y);
+			float absY1 = Mathf.Abs(pos1.y);
+			float ySize = Mathf.Max (absY0,absY1) - Mathf.Min (absY0,absY1);
+			
+			GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+			
+			cube.transform.position = new Vector3((pos0.x+pos1.x)/2f,(pos0.y+pos1.y)/2f,(pos0.z+pos1.z)/2f);
+			cube.transform.localScale = new Vector3(xSize/2f,ySize/2f,16);
+			
+			cube.GetComponent<BoxCollider>().isTrigger = true;
+			cube.AddComponent("ground");
+			cube.GetComponent<ground>().scoreKeeper = scoreKeeper.transform;
+			platforms.Add(cube);
+			
+			
+			if(platforms.Count == numPlatformsAllowed){
+				platformInfo.text = "Out of platforms";
+			}
+			else if((platforms.Count % 2 == 0)){
+				cube.GetComponent<Renderer>().renderer.material = cyan;
+				platformInfo.text = (numPlatformsAllowed-platforms.Count)+" pfrms left, drawing red";
+			}
+			else {
+				cube.GetComponent<Renderer>().renderer.material = red;
+				platformInfo.text = (numPlatformsAllowed-platforms.Count)+" pfrms left, drawing cyan";
+			}
+			
+		}
+	}
+	
+	void recordPositions()
+	{
+		//Vector3 loc = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		Vector3 loc = Camera.main.transform.position + Camera.main.transform.forward * 10f;
+		platformLoc.Add(loc);
+		if((platformLoc.Count % 2) == 0 && platforms.Count < numPlatformsAllowed)
+		{
+			drawPlatform();
 		}
 	}
 }
